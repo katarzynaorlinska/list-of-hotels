8863b34a317c2eeb29e48a54fd15f6792462d6e8
Katarzyna Orlinska <orlinska.kat@gmail.com>
2021-01-28 23:38:28 +0100

    Add img and styles

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,8 +2,15 @@ import "./item.css";
 
 function Item(props) {
   return (
-    <li key={props.id}>
-      {props.name} {props.city}
+    <li key={props.id} className="containerHotel">
+      <div className="leftBox">
+        <img src={props.images[0]} alt="hotel" className="photoHotel"></img>
+      </div>
+      <div className="rightBox">
+        <p>{props.name}</p>
+        <p>{props.city}</p>
+        <p>Rate: {props.stars}</p>
+      </div>
     </li>
   );
 }
