ea85b6a0154d87e7723f5252bc0eff83c657eeb0
Katarzyna Orlinska <orlinska.kat@gmail.com>
2021-01-31 19:29:39 +0100

    Rename className - BEM

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,14 +2,14 @@ import "./item.css";
 
 function Item(props) {
   return (
-    <li key={props.id} className="containerHotel">
-      <div className="leftBox">
-        <img src={props.images[0]} alt="hotel" className="photoHotel"></img>
+    <li key={props.id} className="list">
+      <div className="list__leftBox">
+        <img src={props.images[0]} alt="hotel" className="list__photo"></img>
       </div>
-      <div className="rightBox">
-        <p>{props.name}</p>
-        <p>{props.city}</p>
-        <p>Rate: {props.stars}</p>
+      <div className="list__rightBox">
+        <p className="list__content--name">{props.name}</p>
+        <p className="list__content--city">{props.city}</p>
+        <p className="list__content--rate">Rate: {props.stars}</p>
       </div>
     </li>
   );
