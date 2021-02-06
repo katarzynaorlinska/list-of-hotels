8a17bf9f5073c99a33ea03e03cf220f1316ccd22
Katarzyna Orlinska <orlinska.kat@gmail.com>
2021-02-07 00:41:48 +0100

    Update loaded to loading

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 import List from "./components/List";
 import Hotel from "./components/Hotel";
 import Error from "./components/Error";
-import Loaded from "./components/Loaded";
+import Loading from "./components/Loading";
 
 import "./App.css";
 
 function App() {
   const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -18,11 +18,11 @@ function App() {
       .then((res) => res.json())
       .then(
         (res) => {
-          setIsLoaded(true);
+          setIsLoading(false);
           setItems(res.hotels);
         },
         (error) => {
-          setIsLoaded(true);
+          setIsLoading(false);
           setError(error);
         }
       );
@@ -30,8 +30,8 @@ function App() {
 
   if (error) {
     return <Error message={error.message} />;
-  } else if (!isLoaded) {
-    return <Loaded />;
+  } else if (isLoading) {
+    return <Loading />;
   } else {
     return (
       <Router>
---
+++ src/components/Loading/index.js
@@ -0,0 +1,10 @@
+function Loading(props) {
+  return (
+    <div>Loading...</div>
+  );
+}
+
+export default Loading;
+
+
+
