19bc01770db468daf5c8c2c80549ab0868d08f86
Katarzyna Orlinska <orlinska.kat@gmail.com>
2021-02-08 18:00:54 +0100

    Add search

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,19 +1,33 @@
+/* eslint-disable array-callback-return */
+import { useState } from "react";
 import Item from "../Item";
 
 function List(props) {
+  const [search, setSearch] = useState("");
+
   return (
-    <ul>
-      {props.items.map((item) => (
-        <Item
-          key={`hotel-element-main-list-${item.id}`}
-          id={item.id}
-          name={item.name}
-          city={item.location.city}
-          images={item.images}
-          stars={item.stars}
-        />
-      ))}
-    </ul>
+    <div>
+      <input type="text" placeholder="Search hotel.." onChange={event => {setSearch(event.target.value)}}></input>
+      <ul>
+
+        {props.items.filter((item) => {
+          if(search === ""){
+            return item
+          }else if(item.name.toLowerCase().includes(search.toLocaleLowerCase())){
+            return item
+          }
+        }).map((item) => (
+          <Item
+            key={`hotel-element-main-list-${item.id}`}
+            id={item.id}
+            name={item.name}
+            city={item.location.city}
+            images={item.images}
+            stars={item.stars}
+          />
+        ))}
+      </ul>
+  </div>
   );
 }
 
