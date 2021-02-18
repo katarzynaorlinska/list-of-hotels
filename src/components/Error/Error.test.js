4764eda05adb38c9cb1f0b0440a2b514fc301470
Dawid Urbas <durbas92@gmail.com>
2021-02-18 18:20:07 +0100

    Add test case

---
+++ src/components/Error/Error.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import Error from "./index";
+
+const message = "example message";
+
+test("reder error message", () => {
+  render(<Error message={message} />);
+  const errorElement = screen.getByText(`Error: ${message}`);
+  expect(errorElement).toBeInTheDocument();
+});
+
+test("renders correctly", () => {
+  const tree = renderer.create(<Error message={message} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
